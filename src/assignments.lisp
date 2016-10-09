4d161318d8cb9e3d0074b9697320254a31f1020e
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2016-10-09 21:30:43 +0900

    mbp2 in front of xebio

--- src/assignments.lisp
+++ src/assignments.lisp
@@ -10,7 +10,10 @@
 ;;            (:input :type "submit"))))
 
 (defvar *cookie* "robocar-2016")
+(defvar *cols* 60)
+(defvar *rows* 20)
 
+;; BUG?
 (define-easy-handler (login :uri "/assignments/login") ()
   (multiple-value-bind (sid dummy) (authorization)
     (cond
@@ -22,20 +25,22 @@
 (define-easy-handler (assignments-new :uri "/assignments/new") ()
   (let ((sid (cookie-in *cookie*)))
     (cond
-      ((null sid) (redirect "/assignments/login"))
+      ((or (null sid) (string= "NIL" sid))
+       (multiple-value-bind))
       (t (standard-page
            (:title "Assignments:new")
            (:form :method "post" :action "/assignments/create"
-                  (:input :name "sid" :value sid)
+                  (:p "sid" (:input :name "sid" :value sid))
                   (:p "subject" (:input :name "subject"))
-                  (:textarea :name "answer")
+                  (:textarea :name "answer" :rows *rows* :cols *cols*)
                   (:br)
                   (:input :type "submit")))))))
 
 (define-easy-handler (assignments-create :uri "/assignments/create")
-    (subject answer)
+    (sid subject answer)
   (standard-page
       (:title "Assignment:Create")
+    (:p "sid: " (str sid))
     (:p "subject: " (str subject))
     (:p "answer: " (str answer))
     (:p (:a :href "new" "back")) ; must be changed.
